6440f12c240fd378444ec6a6d5603afbaeab631b
Priyanka <priyanka3007@gmail.com>
2017-01-26 13:23:06 -0800

    Added create and index method used sessions

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,17 @@
 class WordsController < ApplicationController
   def index
+  	
+  	if !session[:views]
+  		session[:views]=0
+  		
+  	end
+  end
+
+  def create
+  	session[:random_word] = Array.new(14).map { (65 + rand(26)).chr }.join
+  	session[:views]=session[:views]+1
+  	redirect_to action: "index"
   end
 end
+
+
